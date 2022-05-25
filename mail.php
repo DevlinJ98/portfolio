3bdda9f63491a1dda053e861aacd106e7fca2a59
DevlinJ98 <devlinj21@gmail.com>
2022-05-25 11:20:45 -0500

    made changes

--- mail.php
+++ mail.php
@@ -21,10 +21,10 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "example@example.com";
+        $recipient = "devlinj21@gmail.com";
 
         // Set the email subject.
-        $subject = "New contact from $name";
+        $subject = "New portfolio contact from $name";
 
         // Build the email content.
         $email_content = "Name: $name\n";
